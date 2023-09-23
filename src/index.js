3458465d4f8931ce8a02946ac1f08652a43e06da
gopalagarwal77 <agarwalgopal247@gmail.com>
2023-09-23 23:21:42 +0530

    body

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express=require("express");
+const bodyParser=require("body-parser");
+
 const { port } = require('./config/serverConfig');    
   //require("dotenv").config();
 //const port = process.env.port || 4000;
@@ -8,6 +10,10 @@ const setupAndStartServer =async() =>{
 
     const app=express(); 
    // const PORT=3000;
+   app.use(bodyParser.json());
+   app.use(bodyParser.urlencoded({extended: true}));
+
+
     app.listen(port ,() =>{
        console.log(`server is running on port ${port}`);
 //console.log(process.env);
